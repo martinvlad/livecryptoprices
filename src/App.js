a024a43fe9b99409c2337b7cc41e8911a4f618d6
Martin Vladimirov <martinvlad@MacBook-Pro.local>
2021-02-15 22:27:37 -0800

    added new changes

--- src/App.js
+++ src/App.js
@@ -1,85 +1,113 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, {  useState, useEffect } from 'react';
 import './App.css';
+import {Dimmer, Loader, Select, Card} from 'semantic-ui-react';
+import Chart from 'react-apexcharts';
+import 'semantic-ui-css/semantic.min.css'
 import Btc from './btc/Btc';
 import Usd from './btc/USD';
 import bitcoin from './bitcoin.png';
 
+const options = [{value: "USD", text: "USD"},{value: "EUR", text: "EUR"},{value: "GBP", text: "GBP"} ]
+
 function App() {
-  const [price, setPrice] = useState(0)
-  const [speed, setspeed] = useState(5)
-  const [USDprice, setUSDprice] = useState(1)
-  const [cryptos, setCryptos] = useState([])
-  const [amount, setAmount] = useState(1)
-  const [amountInFromCurrency, setAmountInFromCurrency] = useState(true)
+  
+  const [priceData, setPriceData] = useState(null)
+  const [speed, setSpeed] = useState(5);
+  const [currency, setCurrency] = useState("USD");
+  const [chartData, setChartData] = useState(null);
+  const [series, setSeries] = useState(null);
+  const [loading, setLoading] = useState(true)
+
+  
   const mainurl = 'https://api.coindesk.com/v1/bpi/currentprice.json';
   
-  let toAmount,fromAmount ;
-if(amountInFromCurrency){
   
-  fromAmount = amount * price;
-  toAmount  = price / price;
-}
-else{
-  toAmount = USDprice;
-  fromAmount = USDprice * price;
-  
-  
-  
-}
   
 
   useEffect(()=> {
-    fetch(mainurl)
-    .then(res => res.json())
-    .then(data =>{  
-      console.log(data)
-      const USDprice = data.bpi.USD.rate_float;
-      
-      setPrice(USDprice)
-      
-    })
-    
-      
-    }, [])
 
-    function handleFromAmountChange(e){
-      setAmount(e.target.value)
-      setAmountInFromCurrency(true)
-      
-      }
-      function handleToAmountChange(e){
-        setAmount(e.target.value)
-        setAmountInFromCurrency(false)
-       
-        
+    async function fetchData() {
+    const res = await fetch(mainurl);
+      const data = await res.json();
+      setPriceData(data.bpi);
+      getChartData();
+    }
+    fetchData();
+  }, []);
+
+   const handleSelect = (e, data) => {
+    setCurrency(data.value)
+   }
+
+   const getChartData = async() =>{
+     const res = await fetch("https://api.coindesk.com/v1/bpi/historical/close.json")
+     const data = await res.json();
+     const categories = Object.keys(data.bpi);
+     const series = Object.values(data.bpi);
+     setChartData({
+       xaxis: {
+         categories: categories
+       }
+     })
+     setSeries([
+        {
+       name: "Bitcoin Price",
+       data: series
         }
-    console.log(price + ' price is the btc price')
-    console.log(USDprice + 'USDPrice is the USDprice')
+     ])
+     setLoading(false);
+   }
+    
   return (
-    <div className="main">
-      
-  <h1>The current price of Bitcoin is  {`  $${price.toFixed(2)}`}</h1>
-  
-  <h5>Time is the ultimate luxury...HODL 4 Life</h5>
-  <img style={{animation: `spin ${speed}s linear infinite`}} src={bitcoin} alt="bitcoin" className="logo" width="150px" />;
-      <div className="box">
-      <Usd
-      name="USD" 
-      price={price} 
-      //onChangeAmount = {handleFromAmountChange}
-      amount={fromAmount}
-      />
-
-      <h1>   =   </h1>
-
-      <Btc 
-      name="BTC" 
-      price={USDprice} 
-      //onChangeAmount = {handleToAmountChange}
-      amount={toAmount}
-      />
+    
+    <div className="App"  >
+            <div className="nav" style={{padding: 15, backgroundColor: "orange"}}>
+        CoinDesk API - BTC Price
+           </div>
+           <div style={{alignItems: "center", textAlign: "center", padding:15}}>
+           <img style={{animation: `spin ${speed}s linear infinite` }} src={bitcoin} alt="bitcoin" className="logo" width="150px" />;
+           </div>
+           
+    {
+    loading ? (
+      <div>
+        <Dimmer active inverted> 
+        <Loader>Loading</Loader>
+        
+        </Dimmer>
       </div>
+    ) : ( 
+      <>
+    <div className="price-container" style={{ 
+      display: "flex",
+     justifyContent: "space-around",
+      alignItems: "center", 
+      width: 600, 
+      height: 100, 
+      margin: "0 auto"
+      }}> 
+              <div className="form">
+      <Select
+      placeholder="Select currency" 
+      onChange={handleSelect} 
+      options={options}></Select>
+              </div>
+              <div className="price">
+                <Card>
+                  <Card.Content>
+    <Card.Header>{currency} Currency</Card.Header>
+    <Card.Description>{priceData[currency].rate}</Card.Description>
+                  </Card.Content>
+                  
+                </Card>
+              </div>
+             
     </div>
+    <div style={{display: "flex", justifyContent: "center", paddingBottom: "20px", margin: 0}}> 
+    <Chart options={chartData} series={series} type="line" height='400' width='800' /></div>
+   </>
+    )}
+   </div>
   );
 }
 
